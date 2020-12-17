3c436db68f0a8eb58457e354cbe98fd86612c4ab
melissikiss <melissikiss@gmail.com>
2020-12-17 12:15:40 -0800

    created DELETE route for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -98,7 +98,21 @@ router.put('/:id', (req, res) => { // update a tag's name by its `id` value
 
 // /api/tags/:id
 router.delete('/:id', (req, res) => { // delete on tag by its `id` value
-
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(tagData => {
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found with this id" });
+      return;
+    }
+    res.json(tagData);
+  })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
