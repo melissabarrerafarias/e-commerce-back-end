a88d80c6663c56ad6721b6169605179cb3b9f2ab
melissikiss <melissikiss@gmail.com>
2020-12-17 12:05:21 -0800

    added get route for tags with specific id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -31,7 +31,36 @@ router.get('/', (req, res) => { // find all tags, be sure to include its associa
 
 // /api/tags/:id
 router.get('/:id', (req, res) => { // find a single tag by its `id`, be sure to include its associated Product data
-
+Tag.findOne({
+  attributes: [
+    'id', 
+    'tag_name'
+  ], 
+  where: {
+    id: req.params.id
+  },
+  include: [
+    {
+      model: Product,
+      attributes: [
+        'product_name',
+        'price',
+        'stock'
+      ],
+      through: ProductTag,
+      as: 'product_info'
+    }
+  ]
+}).then(tagData => {
+  if (!tagData) {
+    res.status(404).json({ message: 'No tag found with this id' });
+    return;
+  }
+  res.json(tagData);
+}).catch(err => {
+  console.log(err);
+  res.status(500).json(err);
+});
 
 });
 
