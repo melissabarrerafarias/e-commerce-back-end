fdeb221e24d476e1a0e207bc685f39f7f71b7799
melissikiss <melissikiss@gmail.com>
2020-12-16 17:45:39 -0800

    added post route for creating a new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -57,8 +57,16 @@ router.get('/:id', (req, res) => { // find one category by its `id` value, be su
   });
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// /api/categories (POST)
+router.post('/', (req, res) => {  // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then(categoryData => res.json(categoryData))
+    .catch(err => {
+      console.log(err);
+      res.json(400).json(err);
+    })
 });
 
 router.put('/:id', (req, res) => {
